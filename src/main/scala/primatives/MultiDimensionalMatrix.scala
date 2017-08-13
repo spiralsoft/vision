02e5972384ec2d0b7153f2b39a6ec23ddd3e6f6d
Shea Hawkins <shea.m.hawkins@gmail.com>
2017-08-13 07:37:26 -0700

    Add methods to create matrix from tango-format

--- src/main/scala/primatives/MultiDimensionalMatrix.scala
+++ src/main/scala/primatives/MultiDimensionalMatrix.scala
@@ -37,19 +37,136 @@ class MultiDimensionalMatrix (
 
   val data: DenseVector[Double] = DenseVector[Double](input_data)
 
-  def getArrayIndexRowMajor(index: Seq[Int]): Int =
+  val getIndex: (Seq[Int]) => Int = {
+    index_type match {
+      case IndexType.ColumnMajor => MultiDimensionalMatrix.getArrayIndexColumnMajor
+      case IndexType.RowMajor    => MultiDimensionalMatrix.getArrayIndexRowMajor
+    }
+  }
+
+
+}
+
+
+object MultiDimensionalMatrix {
+  /**
+    * Generates a multidimensional matrix from a flattened array of
+    * (coordinate, ..., coordinate -> value, ..., value) / (value, ..., value -> coordinate, ..., coordinate) pairs
+    *
+    * @param dim The dimension of the target matrix.
+    * @param arr Flattened array of Seq[coordinate] --> Seq[value] pairs. The number of values per coordinate is the final dimension
+    *            of the array.
+    */
+  def apply(dim: Array[Int], arr: Seq[Double], index_type: IndexType.IndexType = IndexType.RowMajor): MultiDimensionalMatrix = {
+
+    val pair_length = {
+      index_type match {
+        case IndexType.RowMajor    => dim.length - 1 + dim.last
+        case IndexType.ColumnMajor => dim.length - 1 + dim.head
+      }
+    }
+
+    apply(dim, arr.grouped(pair_length).toIterable, index_type)
+
+  }
+  
+
+  private def generateCoordValDataArrayRowMajor(dim: Array[Int], points: Iterable[Iterable[Double]]): Array[Double] = {
+    val size = getArrayIndexRowMajor(dim)
+
+    val num_coords  = dim.length - 1
+    val num_vals    = dim.last
+    val pair_length = num_coords + num_vals
+
+    val data_array = Array.fill[Double](size)(null)
+
+    // Indexes the data array appropriately, filling the coordinate spaces with the values of the last dimension
+    points.par.foreach(
+      (point: Iterable[Double]) => {
+
+        // Gives the coordinate excluding the last value
+        val coord = point.slice(0, num_coords + 1).map(double => double.toInt)
+        val index = getArrayIndexColumnMajor(coord)
+
+        val values = point.slice(num_coords + 1, pair_length)
+
+        // Adds the final dimension position to the current index and
+        // sets the value at that index equal to the retrieved value
+        var value_ind = 0
+        values.foreach((point_value) => {
+          data_array(index + value_ind) = point_value
+          value_ind = value_ind + 1
+        })
+
+      }
+    )
+
+    data_array
+  }
+
+  private def generateCoordValDataArrayColumnMajor(dim: Array[Int], points: Iterable[Iterable[Double]]): Array[Double] = {
+    val size = getArrayIndexColumnMajor(dim)
+
+    val num_coords  = dim.length - 1
+    val num_vals    = dim.head
+    val pair_length = num_coords + num_vals
+
+    val data_array = Array.fill[Double](size)(null)
+
+    // Indexes the data array appropriately, filling the coordinate spaces with the values of the last dimension
+    points.par.foreach(
+      (point: Iterable[Double]) => {
+
+        // Gives the coordinate excluding the values
+        val coord = Seq(0) ++ point.slice(num_vals + 1, num_coords + 1).map(double => double.toInt)
+        val index = getArrayIndexColumnMajor(coord)
+
+        val values = point.slice(0, num_vals + 1)
+
+        // Adds the first dimension position to the current index and
+        // sets the value
+        var value_ind = 0
+        values.foreach((point_value) => {
+          data_array(index + value_ind) = point_value
+          value_ind = value_ind + 1
+        })
+
+      }
+    )
+
+    data_array
+  }
+
+
+  /**
+    * Generates a multidimensional matrix from an array of arrays that contain coordinate, ..., coordinate, value, ..., values
+    * Handles pointclouds with variable number of C->RGB->Any
+    * @param points
+    */
+  def apply(dim: Array[Int], points: Iterable[Iterable[Double]], index_type: IndexType.IndexType = IndexType.RowMajor): MultiDimensionalMatrix = {
+    val data_array: Array[Double] = {
+      index_type match {
+        case IndexType.ColumnMajor => generateCoordValDataArrayColumnMajor(dim, points)
+        case IndexType.RowMajor    => generateCoordValDataArrayRowMajor(dim, points)
+      }
+    }
+
+    new MultiDimensionalMatrix(dim, data_array, index_type)
+  }
+
+  private def getArrayIndexRowMajor(index: Iterable[Int]): Int =
   {
-    index.zipWithIndex.foldLeft(0)[Int]{
+    index.view.zipWithIndex.foldLeft(0)[Int]{
       case (accum: Int, index_dim: (Int, Int)) => index_dim._1 + index_dim._2 * accum
     }
   }
 
 
-  def getArrayIndexColumnMajor(index: Seq[Int]): Int =
+  private def getArrayIndexColumnMajor(index: Iterable[Int]): Int =
   {
-    index.zipWithIndex.foldRight(0)[Int]{
+    index.view.zipWithIndex.foldRight(0)[Int]{
       case (index_dim: (Int, Int), accum: Int) => index_dim._1 + index_dim._2 * accum
     }
   }
-
 }
+
