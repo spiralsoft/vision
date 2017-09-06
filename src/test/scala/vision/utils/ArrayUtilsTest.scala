ad87c8cba5c34ae7ee0ba9cefea8b11e9225e56f
Shea Hawkins <shea.m.hawkins@gmail.com>
2017-09-06 09:25:12 -0700

    Add array util tests

---
+++ src/test/scala/vision/utils/ArrayUtilsTest.scala
@@ -0,0 +1,43 @@
+package vision.utils
+
+import org.scalatest.{FlatSpec, Matchers}
+import vision.utils.ArrayUtils.{nanArrayFloat, nanArrayDouble}
+import scala.util.Random
+/**
+  * Created by shea on 9/6/17.
+  */
+class ArrayUtilsTest extends FlatSpec with Matchers {
+  "A nanFloat array" should "generate an array of designated size" in {
+    val size = 100
+    val arr = nanArrayFloat(size)
+    arr.length should be (size)
+  }
+
+  it should "be prepopulated with nans" in {
+    val size = 50
+    val arr = nanArrayFloat(size)
+
+    val ind = Random.nextInt(size)
+    val rand_comp = Random.nextInt(100)
+
+    arr(ind).isNaN should be (true)
+    arr(ind) should not be rand_comp
+  }
+
+  "A nanDouble array" should "generate a nanDouble array of designated size" in {
+    val size = 100
+    val arr = nanArrayDouble(size)
+    arr.length should be (size)
+  }
+
+  it should "be prepopulated with nans" in {
+    val size = 50
+    val arr = nanArrayDouble(size)
+
+    val ind = Random.nextInt(size)
+    val rand_comp = Random.nextInt(100)
+
+    arr(ind).isNaN should be (true)
+    arr(ind) should not be rand_comp
+  }
+}
---
+++ src/test/scala/vision/utils/structures/MultiDimensionalMatrixTest.scala
@@ -0,0 +1,8 @@
+package vision.utils.structures
+
+/**
+  * Created by shea on 9/6/17.
+  */
+class MultiDimensionalMatrixTest {
+
+}
