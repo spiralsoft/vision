b6068b0b76491577bb5fbd9c48917ffbe052658f
Shea Hawkins <shea.m.hawkins@gmail.com>
2017-09-21 19:44:56 -0700

    Add test for 4D MDM

--- src/test/scala/vision/structures/MultiDimensionalMatrixTest.scala
+++ src/test/scala/vision/structures/MultiDimensionalMatrixTest.scala
@@ -80,4 +80,37 @@ class MultiDimensionalMatrixTest extends FlatSpec with Matchers {
   }
 
 
+  behavior of "A four dimensional MDM"
+
+  val row_num_4   = 7
+  val col_num_4   = 14
+  val depth_num_4 = 21
+  val dim_4       = 4
+
+  val doubles_4d: Seq[Seq[Seq[Seq[Double]]]] =
+    Seq.fill[Seq[Seq[Seq[Double]]]](row_num_4)(
+      Seq.fill[Seq[Seq[Double]]](col_num_4)(
+        Seq.fill[Seq[Double]](depth_num_4)(
+          Seq.fill[Double](dim_4)(Math.random())
+        )
+      )
+    )
+
+  val mat_4d: MultiDimensionalMatrix[Double] = MultiDimensionalMatrix[Double](doubles_4d, MatrixIndexType.ColumnMajor)
+
+  it should "be created column-major with appropriate dimensions from a sequence of sequences." in {
+    mat_4d.dim.length should be (4)
+    mat_4d.dim.head should be (row_num_4)
+    mat_4d.dim(1)   should be (col_num_4)
+    mat_4d.dim(2) should be (depth_num_4)
+    mat_4d.dim.last should be (dim_4)
+  }
+
+  it should "contain the correct values in the correct positions" in {
+
+    mat_4d.getAtIndex(Seq(1, 1, 1, 1)) should be (doubles_4d(1)(1)(1)(1))
+    mat_4d.getAtIndex(Seq(4, 4, 3, 0)) should be (doubles_4d(4)(4)(3)(0))
+
+  }
+
 }
